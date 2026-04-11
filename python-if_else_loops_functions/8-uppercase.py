e4775e7c8cda0d2a311a02aaf6a2670b0426bbe2
Fatima Sultanova <144486053+fatimasultanova@users.noreply.github.com>
2026-04-11 13:59:16 +0400

    Implement uppercase function in 8-uppercase.py
    Add function to convert string to uppercase and print it.

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        temp = i
+        if ord(i) >= ord('a') and ord(i) <= ord('z'):
+            temp = chr(ord(i) - 32)
+        print("{}".format(temp), end="")

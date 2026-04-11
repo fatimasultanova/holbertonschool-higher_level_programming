0c88c4972a2c2ba3f13d3fcc4a8dddcf81a256b0
Fatima Sultanova <144486053+fatimasultanova@users.noreply.github.com>
2026-04-11 23:32:13 +0400

    Fix formatting of modulus operator in divisible_by_2

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     new_ls = []
     for i in my_list:
-        if i%2 == 0:
+        if i % 2 == 0:
             new_ls.append(True)
         else:
             new_ls.append(False)

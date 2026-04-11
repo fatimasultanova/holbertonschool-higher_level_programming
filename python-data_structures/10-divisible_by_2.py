73f7314d6f7083535048a92b22c92057d3ad3e4c
Fatima Sultanova <144486053+fatimasultanova@users.noreply.github.com>
2026-04-11 23:30:18 +0400

    Add function to check divisibility by 2

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    new_ls = []
+    for i in my_list:
+        if i%2 == 0:
+            new_ls.append(True)
+        else:
+            new_ls.append(False)
+    return new_ls

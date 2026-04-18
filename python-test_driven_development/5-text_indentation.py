a13eec29b1fb8de6d6c186244e05780e00932261
Fatima Sultanova <144486053+fatimasultanova@users.noreply.github.com>
2026-04-18 23:49:29 +0400

    Add text_indentation function for text formatting
    Implement text_indentation function to format text with new lines after specific punctuation.

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This module provides the text_indentation function.
+"""
+
+
+def text_indentation(text):
+    """
+    Prints text with two new lines after each '.', '?', and ':'.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    i = 0
+    while i < len(text) and text[i] == ' ':
+        i += 1
+
+    while i < len(text):
+        print(text[i], end="")
+        if text[i] in ".?:":
+            print("\n")
+            i += 1
+            while i < len(text) and text[i] == ' ':
+                i += 1
+            continue
+        i += 1

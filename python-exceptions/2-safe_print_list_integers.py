95aeb694073536756c37da290c19c9ecddf4a455
Fatima Sultanova <144486053+fatimasultanova@users.noreply.github.com>
2026-04-18 14:44:42 +0400

    Remove IndexError handling in safe_print_list_integers
    Removed the IndexError exception handling from the function.

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -7,7 +7,5 @@ def safe_print_list_integers(my_list=[], x=0):
             nb_printed += 1
         except (ValueError, TypeError):
             pass
-        except IndexError:
-            break
     print("")
     return nb_printed

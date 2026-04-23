3acf8ac4eb7d705ab0e5b55e89d558e11352d4b2
Fatima Sultanova <fatimasultanova.909@gmail.com>
2026-04-23 23:22:35 +0400

    1

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module defines an empty class Square.
+The Square class is used to represent a geometric square shape.
+"""
+
+
+class Square:
+    """A class that defines a square."""
+    def __init__(self, size):
+        """Initialize a Square object."""
+        self.__size = size

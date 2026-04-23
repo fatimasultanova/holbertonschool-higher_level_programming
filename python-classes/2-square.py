1db6e2ebddc054f269ad2bfb43e1ad6b93bcd294
Fatima Sultanova <fatimasultanova.909@gmail.com>
2026-04-23 23:37:10 +0400

    2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -7,10 +7,13 @@ The Square class is used to represent a geometric square shape.
 
 class Square:
     """A class that defines a square. """
+
     def __init__(self, size=0):
-        """Initialize a Square object. """
+        """
+        Initialize a Square object.
+        """
         if not isinstance(size, int):
-            raise TypeError('size must be an integer')
+            raise TypeError("size must be an integer")
         elif size < 0:
-            raise ValueError('size must be >= 0')
+            raise ValueError("size must be >= 0")
         self.__size = size

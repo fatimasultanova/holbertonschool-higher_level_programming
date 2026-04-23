7826f8477e780262f5156d62d8cebea0ca3e0d27
Fatima Sultanova <fatimasultanova.909@gmail.com>
2026-04-23 23:28:16 +0400

    2

---
+++ python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module defines an empty class Square.
+The Square class is used to represent a geometric square shape.
+"""
+
+
+class Square:
+    """A class that defines a square."""
+    def __init__(self, size = 0):
+        """Initialize a Square object."""
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.size = size

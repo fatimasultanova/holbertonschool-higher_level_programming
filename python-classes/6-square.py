56511ff76cc4625247b4e1d35b0bbfd99199f60d
Fatima Sultanova <fatimasultanova.909@gmail.com>
2026-04-24 00:43:42 +0400

    so

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -74,5 +74,5 @@ class Square:
         for i in range(self.__position[1]):
             print("")
 
-        for i in range(self.__size):
+        for _ in range(self.__size):
             print(" " * self.__position[0] + "#" * self.__size)

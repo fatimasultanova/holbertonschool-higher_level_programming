3c6b44de1fd6d75d39622f47c85969c965f6d0a8
Fatima Sultanova <fatimasultanova.909@gmail.com>
2026-04-24 10:13:32 +0400

    1

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -6,4 +6,31 @@ This module defines an empty class Square.
 
 class Rectangle:
     """Rectangle class is used to represent a geometric square shape."""
-    pass
+
+
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.width
+    @property
+    def height(self):
+        return self.height
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value

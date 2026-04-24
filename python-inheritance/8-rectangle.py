f14165c462676f5ba5515cb310c19cd005e508a1
Fatima Sultanova <fatimasultanova.909@gmail.com>
2026-04-25 00:42:54 +0400

    8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -5,8 +5,11 @@ Rectangle class created
 
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
 class Rectangle(BaseGeometry):
     """Rectangle class"""
+
+
     def __init__(self, width, height):
         """Initialize Rectangle class
         Args:

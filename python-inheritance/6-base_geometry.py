daac79e8f1dba11b8b47ae468c162962285e7400
Fatima Sultanova <fatimasultanova.909@gmail.com>
2026-04-24 23:54:30 +0400

    6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Geometry class created
+"""
+
+
+class BaseGeometry:
+    """Geometry class"""
+    def area(self):
+        """Calculate area of BaseGeometry"""
+        raise Exception('area() is not implemented')

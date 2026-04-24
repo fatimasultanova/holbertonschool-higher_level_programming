3c70dd94dd089c5a988ed09d5f847b4ebe52d961
Fatima Sultanova <fatimasultanova.909@gmail.com>
2026-04-24 23:39:03 +0400

    3

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -5,5 +5,5 @@ Python Inheritance Rules
 
 
 def is_kind_of_class(obj, a_class):
-    """Check if a class is a subclass of another class"""
-    return type(obj) is a_class or issubclass(obj, a_class)
+    """Check if a class is instance of another class"""
+    return isinstance(obj, a_class)

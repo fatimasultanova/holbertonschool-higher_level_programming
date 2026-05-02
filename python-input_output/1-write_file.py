4eb97a8376f932746c4f606de75bfe7e63167760
Fatima Sultanova <fatimasultanova.909@gmail.com>
2026-05-02 19:52:04 +0400

    1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+"""
+It includes a function for writing text to a file in append mode, ensuring that
+the text is encoded in UTF-8.
+"""
 def write_file(filename='', text=''):
+    """
+        Write text to a file
+    Args:
+        filename:
+        text:
+    """
     with open(filename, 'a',encoding="utf-8") as file:
         file.write(text)
         return len(text)

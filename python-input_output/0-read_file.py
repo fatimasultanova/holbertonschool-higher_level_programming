acca3c53994baffd43da4ec06c60a9452865c3e3
Fatima Sultanova <fatimasultanova.909@gmail.com>
2026-05-02 19:42:08 +0400

    0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def read_file(file_name=''):
-    with open(file_name, 'r') as file:
-        print(file.read())
+    with open(file_name, "r", encoding="utf-8") as f:
+        print(f.read(), end="")

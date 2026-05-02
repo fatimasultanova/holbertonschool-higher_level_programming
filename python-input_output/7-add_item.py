b5c9b21d547aee50882ddfe0820a1cb0cc0a8103
Fatima Sultanova <fatimasultanova.909@gmail.com>
2026-05-02 20:33:17 +0400

    7

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This script adds all command-line arguments to a Python list
+and saves them to a JSON file named add_item.json.
+"""
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+def add_item_to_list():
+    """
+    Loads existing list, appends new arguments, and saves back to JSON.
+    """
+    filename = "add_item.json"
+    items = load_from_json_file(filename)
+    save_to_json_file(items, filename)

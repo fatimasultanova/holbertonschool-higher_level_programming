40bc17bb598da8e30768ff02f15bfbf234a652cf
Fatima Sultanova <fatimasultanova.909@gmail.com>
2026-05-02 20:36:09 +0400

    7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,6 +5,7 @@ and saves them to a JSON file named add_item.json.
 """
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+import sys
 
 
 def add_item_to_list():
@@ -13,4 +14,5 @@ def add_item_to_list():
     """
     filename = "add_item.json"
     items = load_from_json_file(filename)
+    items.extend(sys.argv[1:])
     save_to_json_file(items, filename)

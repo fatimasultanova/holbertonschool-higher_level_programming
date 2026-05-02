702473cd2f915d973b9f231475a5767bf8685186
Fatima Sultanova <fatimasultanova.909@gmail.com>
2026-05-02 23:58:18 +0400

    03

---
+++ python-serialization/task_03_xml.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import xml.etree.ElementTree as ET
+
+
+def serialize_to_xml(dictionary, filename):
+    root = ET.Element("data")
+
+    for key, value in dictionary.items():
+        child = ET.SubElement(root, key)
+        child.text = str(value)
+
+    tree = ET.ElementTree(root)
+    tree.write(filename, encoding='utf-8', xml_declaration=True)
+
+
+def deserialize_from_xml(filename):
+    try:
+        tree = ET.parse(filename)
+        root = tree.getroot()
+
+        data_dict = {}
+
+        for child in root:
+            data_dict[child.tag] = child.text
+        return data_dict
+    except FileNotFoundError:
+        return None

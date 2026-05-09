e1e160a5698fd042e7600a3881aff7d4f4531472
Fatima Sultanova <fatimasultanova.909@gmail.com>
2026-05-09 18:19:27 +0400

    3

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -38,7 +38,7 @@ class SimpleAPIHandler(http.server.BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b"404 Not Found: Endpoint not found")
+            self.wfile.write(b"Endpoint not found")
 
 with socketserver.TCPServer(("", PORT), SimpleAPIHandler) as httpd:
     httpd.serve_forever()
